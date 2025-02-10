12720083774103d055f34c51a7662691ac17ec3c
JesnaSebastian <jesnasebastiankottayil@gmail.com>
2025-02-10 11:34:52 +0530

    Home page object class

---
+++ src/test/java/testCases/TC_01_Homepage.java
@@ -0,0 +1,23 @@
+package testCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import pageObjects.homePage;
+import testBase.baseClass;
+
+public class TC_01_Homepage extends baseClass {
+    @Test
+    public void TC01_homepage() throws InterruptedException {
+
+
+        homePage hp2=new homePage(driver);
+        hp2.click_myaccount();
+        hp2.click_reglink();
+        Thread.sleep(5000);
+    }
+
+
+    }
+
