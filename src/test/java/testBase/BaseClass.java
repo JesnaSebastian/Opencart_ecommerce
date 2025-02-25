eb782194a270fc14bbc92548068621258290d2d1
JesnaSebastian <jesnasebastiankottayil@gmail.com>
2025-02-25 10:54:31 +0530

    After adding SS method

---
+++ src/test/java/testBase/BaseClass.java
@@ -0,0 +1,53 @@
+package testBase;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.Date;
+
+
+public class BaseClass {
+   public static WebDriver driver;
+   public Logger logger;
+    @BeforeClass
+    public void launch_browser()
+    {
+        logger = LogManager.getLogger(this.getClass());
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://tutorialsninja.com/demo");
+
+    }
+
+    public String captureScreen(String tname) throws IOException {
+
+        String timeStamp = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+
+        TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+        File sourceFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
+
+        String targetFilePath=System.getProperty("user.dir")+"\\screenshots\\" + tname + "_" + timeStamp + ".png";
+        File targetFile=new File(targetFilePath);
+
+        sourceFile.renameTo(targetFile);
+
+        return targetFilePath;
+
+    }
+
+    @AfterClass
+    public void teardown(){
+        driver.close();
+    }
+}
